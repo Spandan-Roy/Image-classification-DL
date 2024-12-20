3873ee09e59c9b65f2a2ec2dc12018f20beb927c
Spandan-Roy <spandan.roy.stats@gmail.com>
2024-12-20 19:33:04 +0530

    Evaluation Stage added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.CNN_Classifier import logger
 from CNN_Classifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_Classifier.pipeline.Stage_02_prepare_model import PrepareBaseModelTrainingPipeline
 from CNN_Classifier.pipeline.Stage_03_model_training import ModelTrainingPipeline
-
+from CNN_Classifier.pipeline.Stage_04_model_evaluation import EvaluationPipeline
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
@@ -35,3 +35,13 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME="Evaluate Model"
+try:
+    logger.info(f"*********")
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj=ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n \n")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/CNN_Classifier/pipeline/Stage_04_model_evaluation.py
@@ -0,0 +1,28 @@
+from CNN_Classifier.config.configuration import ConfigurationManager
+from CNN_Classifier.components.mode_evaluation import Evaluation
+from CNN_Classifier import logger
+
+
+STAGE_NAME = "Evaluation Stage"
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config=ConfigurationManager()
+        eval_config=config.get_evaluation_config()
+        evaluation=Evaluation()
+        evaluation.save_score()
+        evaluation.log_into_mlflow()
+        
+if __name__=="__main__":
+    try:
+        logger.info(f"*********")
+        logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+        obj=EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n \n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
