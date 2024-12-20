fd74788577df4f00c1e45cb900dd43a82fac5828
Spandan-Roy <spandan.roy.stats@gmail.com>
2024-12-20 15:19:08 +0530

    Model trained

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.CNN_Classifier import logger
 from CNN_Classifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_Classifier.pipeline.Stage_02_prepare_model import PrepareBaseModelTrainingPipeline
-
+from CNN_Classifier.pipeline.Stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -23,3 +23,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME="Train Model"
+try:
+    logger.info(f"*********")
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj=ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n \n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
